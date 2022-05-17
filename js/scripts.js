d9c1137b81cece8f3aaddae730bf7e84a539eb59
ladegbaye Stella <Ladegbayeewatomi50@gmail.com>
2022-05-17 16:52:41 +0100

    adding my script which is the functionaliy part

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  $("#form-control").submit(function (e) {
+    e.preventDefault();
+
+    let input1 = $("#carrot").val().toUpperCase()
+    let input2 = $("#milk").val().toUpperCase()
+    let input3 = $("#Tomatoes").val().toUpperCase()
+    let input4 = $("#Eggs").val().toUpperCase()
+    let input5 = $("#Apples").val().toUpperCase()
+    let input6 = $("#onions").val().toUpperCase()
+
+    let newArray = [input1, input2, input3, input4, input5, input6];
+    let newArray1 = newArray.sort();
+
+    $("#form-control").hide();
+
+    newArray1.forEach(function (list){
+      $("#output").append("<li>".concat(list,"<li>"));
+    })
+
+
+  });
+
+
+
+
+
+
+
+});
